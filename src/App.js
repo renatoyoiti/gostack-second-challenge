30150080d7fcece2d8525fcd1e06818819c44024
= <=>
2020-09-19 19:55:13 -0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+import './styles.css';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
+	useEffect(() => {
+		api.get('/repositories').then((response) => {
+			setRepositories(response.data);
+		});
+	}, []);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	async function handleAddRepository() {
+		const response = await api.post('/repositories', {
+			title: 'gostack-second-challenge',
+			url: 'https://github.com/renatoyoiti/gostack-second-challenge',
+			techs: ['Javascript, ReactJS'],
+		});
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+		setRepositories([...repositories, response.data]);
+	}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleRemoveRepository(id) {
+		await api.delete(`/repositories/${id}`);
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		setRepositories(repositories.filter((repository) => repository.id !== id));
+	}
+
+	return (
+		<div>
+			<ul data-testid='repository-list'>
+				{repositories.map((repository) => (
+					<li key={repository.id}>
+						{repository.title}
+						<button onClick={() => handleRemoveRepository(repository.id)}>
+							Remover
+						</button>
+					</li>
+				))}
+			</ul>
+
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
